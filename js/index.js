c0f89097e8aa75a095f28ebdedf9e35f888273d1
Daniel <garoz.daniel@gmail.com>
2016-03-18 00:04:15 +0100

    Start proyect

---
+++ js/index.js
@@ -0,0 +1,54 @@
+var date = new Date();
+date.setDate(24);
+date.setHours(17);
+date.setMinutes(0);
+date.setSeconds(0);
+
+
+setInterval(function(){
+		var time = date - new Date();
+		var dias = Math.floor(time / 3600000 / 24);
+		if(dias >= 1){
+			$("#dias").html(dias);
+			if(dias == 1) {
+				$("#dias-text").html("dia");
+			}else{
+				$("#dias-text").html("dias");
+			}
+			time -= dias * 3600000 * 24;
+		} else {
+			$("#dias").html(0);
+		}
+		var horas = Math.floor(time / 3600000);
+		if(horas >= 1){
+			$("#horas").html(horas);
+			if(dias == 1) {
+				$("#horas-text").html("hora");
+			}else{
+				$("#horas-text").html("horas");
+			}
+			time -= horas * 3600000;
+		} else {
+			$("#horas").html(0);
+		}
+		var minutos = Math.floor(time / 60000);
+		if(minutos >= 1){
+			$("#minutos").html(minutos);
+			if(dias == 1) {
+				$("#minutos-text").html("minuto");
+			}else{
+				$("#minutos-text").html("minutos");
+			}
+			time -= minutos * 60000;
+		} else {
+			$("#minutos").html(0);
+		}
+		var segundos = Math.floor(time / 1000);
+		if(segundos >= 1){
+			$("#segundos").html(segundos);
+			time -= segundos * 1000;
+		} else {
+			$("#segundos").html(0);
+		}
+		$("#milisegundos").html(time);
+	}, 53)
